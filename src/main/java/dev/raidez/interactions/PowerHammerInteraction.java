15b8f4e011cfc7d952af60bd931c2ff09b67ad08
Alexis LUCIEN <alexis.lucien@outlook.fr>
2026-01-29 07:44:30 +0100

    Format json files. Use explicit types instead of var.

--- src/main/java/dev/raidez/interactions/PowerHammerInteraction.java
+++ src/main/java/dev/raidez/interactions/PowerHammerInteraction.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import com.hypixel.hytale.codec.builder.BuilderCodec;
 import com.hypixel.hytale.component.CommandBuffer;
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.math.vector.Vector3i;
 import com.hypixel.hytale.protocol.BlockSoundEvent;
 import com.hypixel.hytale.protocol.InteractionType;
@@ -37,23 +39,23 @@ public class PowerHammerInteraction extends SimpleBlockInteraction {
             Vector3i targetBlock,
             CooldownHandler cooldownHandler) {
 
-        var ref = context.getEntity();
-        var store = ref.getStore();
-        var player = store.getComponent(ref, Player.getComponentType());
-        var storage = player.getInventory().getHotbar();
-        var heldItemIndex = (short) context.getHeldItemSlot();
+        Ref<EntityStore> ref = context.getEntity();
+        Store<EntityStore> store = ref.getStore();
+        Player player = store.getComponent(ref, Player.getComponentType());
+        ItemContainer storage = player.getInventory().getHotbar();
+        Short heldItemIndex = (short) context.getHeldItemSlot();
 
         // Check if the target block is wood
-        var targetBlockType = world.getBlockType(targetBlock);
-        var targetBlockGroup = Optional.ofNullable(targetBlockType).map(BlockType::getGroup).orElse(null);
+        BlockType targetBlockType = world.getBlockType(targetBlock);
+        String targetBlockGroup = Optional.ofNullable(targetBlockType).map(BlockType::getGroup).orElse(null);
         if (targetBlockType == null || targetBlockGroup == null || !targetBlockGroup.equals("Wood")) {
             PowerHammerPlugin.LOGGER.atWarning().log("Power Hammer can only be used on wood blocks!");
             return;
         }
 
         // Get the first block next to the powerhammer
-        var closestIndex = getClosestBlockType(storage, heldItemIndex, targetBlockType.getId(), "Wood");
-        var selectedBlockIS = Optional.ofNullable(closestIndex).map(storage::getItemStack).orElse(null);
+        Short closestIndex = getClosestBlockType(storage, heldItemIndex, targetBlockType.getId(), "Wood");
+        ItemStack selectedBlockIS = Optional.ofNullable(closestIndex).map(storage::getItemStack).orElse(null);
         if (closestIndex == null || selectedBlockIS == null) {
             PowerHammerPlugin.LOGGER.atWarning().log("No wood blocks found in hotbar!");
             return;
@@ -73,7 +75,7 @@ public class PowerHammerInteraction extends SimpleBlockInteraction {
                 -heldItemStack.getItem().getDurabilityLossOnHit(), commandBuffer);
 
         // Play sound effect
-        var soundSet = BlockSoundSet.getAssetMap().getAsset(targetBlockType.getBlockSoundSetIndex());
+        BlockSoundSet soundSet = BlockSoundSet.getAssetMap().getAsset(targetBlockType.getBlockSoundSetIndex());
         int soundEventIndex = Optional.ofNullable(soundSet).map(BlockSoundSet::getSoundEventIndices)
                 .map((indices) -> indices.getOrDefault(BlockSoundEvent.Hit, 0)).orElse(0);
         if (soundSet == null || soundEventIndex == 0) {
@@ -100,22 +102,22 @@ public class PowerHammerInteraction extends SimpleBlockInteraction {
         Short closestIndex = null;
         Integer closestDistance = null;
         for (short i = 0; i < storage.getCapacity(); i++) {
-            var itemStack = storage.getItemStack(i);
-            var blockId = Optional.ofNullable(itemStack).map(ItemStack::getBlockKey).orElse(null);
+            ItemStack itemStack = storage.getItemStack(i);
+            String blockId = Optional.ofNullable(itemStack).map(ItemStack::getBlockKey).orElse(null);
             if (itemStack == null || blockId == null) {
                 continue;
             }
 
             // Check if the item is a wood block
-            var blockType = BlockType.getAssetMap().getAsset(blockId);
-            var blockGroup = Optional.ofNullable(blockType).map(BlockType::getGroup).orElse(null);
+            BlockType blockType = BlockType.getAssetMap().getAsset(blockId);
+            String blockGroup = Optional.ofNullable(blockType).map(BlockType::getGroup).orElse(null);
             if (blockType == null || blockGroup == null || !blockGroup.equals(group)
                     || blockType.getId().equals(targetBlockId)) {
                 continue;
             }
 
             // Calculate closest block to the powerhammer
-            var distance = Math.abs(i - heldItemIndex);
+            Integer distance = Math.abs(i - heldItemIndex);
             if (closestIndex == null || distance < closestDistance) {
                 closestIndex = i;
                 closestDistance = distance;
